f2418cc592110fae69c6cedcf541e76385ff5ed8
Philip Krook <philip.krook@tietoevry.com>
2021-05-04 11:30:22 +0200

    More fixes and links

--- Constants.cs
+++ Constants.cs
@@ -2,14 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace StatiqBlog
+namespace MySite
 {
     public class Constants
     {
-        public const string SiteTitle = "Your Website";
+        public const string SiteTitle = "Philip Krook";
         public const string SiteUri = "https://example.com";
-        public const string BlogTitle = "Your Blog";
-        public const string BlogPath = "blog"; // ex. "blog" indicates https://example.com/blog/
+        public const string StartPath = "start"; // ex. "blog" indicates https://example.com/blog/
         public const int PostsPerPage = 10;
     }
 }
